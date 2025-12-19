da515afdc88fd753f53285a47d88318ffdfa520f
vuthihoandp-hash <vuthihoandp@gmail.com>
2025-12-19 19:21:04 +0700

    Update app.py

--- app.py
+++ app.py
@@ -191,7 +191,7 @@ st.markdown("""
 st.markdown("""
 <div class="main-header">
     <h1>📘 TRỢ LÝ SOẠN GIÁO ÁN TỰ ĐỘNG (NLS)</h1>
-    <p>Tác giả: Lù Seo Sần - Trường PTDTBT Tiểu học Bản Ngò - ĐT: 097 1986 343</p>
+    <p>Tác giả: Vũ Thị Hoãn - Trường Tiểu học Đông An - ĐT: 0912548351</p>
 </div>
 """, unsafe_allow_html=True)
 
@@ -347,4 +347,4 @@ if st.button("🚀 SOẠN GIÁO ÁN NGAY"):
 
 # --- CHÂN TRANG ---
 st.markdown("---")
-st.markdown("<div style='text-align: center; color: #666;'>© 2025 - Lù Seo Sần - Trường PTDTBT Tiểu học Bản Ngò - ĐT: 097 1986 343</div>", unsafe_allow_html=True)
+st.markdown("<div style='text-align: center; color: #666;'>© 2025 - Vũ Thị Hoãn - Trường Tiểu học Đông An - ĐT: 0912548351</div>", unsafe_allow_html=True)
